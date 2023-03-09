c5785eb18f926c3b3629b665f21bf4250eee6da0
Hancok17 <107268812+Hancok17@users.noreply.github.com>
2023-03-10 06:01:32 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf('6102206407:AAEwnQD1k_YRg3CR5Yla8ikhi9n_yQRYvSQ');
 const port= 8080 || 3000;
-
+const webhookUrl = `https://$https://tgbot-m0zh.onrender.com/bot$6102206407:AAEwnQD1k_YRg3CR5Yla8ikhi9n_yQRYvSQ`;
 bot.start((ctx) => ctx.reply('Hello!'));
 
 bot.help((ctx) => ctx.reply('Send me a message and I will reply!'));
@@ -12,6 +12,9 @@ bot.on('message', (ctx) => {
 
 bot.launch({
   webhook: {
-    port: port
+    port: port,
+    domain :'https://tgbot-m0zh.onrender.com ',
+    hookPath: webhookUrl
+    
   }
 });
