a056ddc50ee51c2669daf1c843046dd10329ef4b
Hancok17 <107268812+Hancok17@users.noreply.github.com>
2023-03-10 03:39:12 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf('6102206407:AAEwnQD1k_YRg3CR5Yla8ikhi9n_yQRYvSQ');
 
+
 bot.start((ctx) => ctx.reply('Hello!'));
 
 bot.help((ctx) => ctx.reply('Send me a message and I will reply!'));
