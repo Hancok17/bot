1758c2dea413e22503067ff0cb4642ae8116892e
Hancok17 <107268812+Hancok17@users.noreply.github.com>
2023-03-10 02:44:22 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.6102206407:AAEwnQD1k_YRg3CR5Yla8ikhi9n_yQRYvSQ);
+
+bot.start((ctx) => ctx.reply('Hello!'));
+
+bot.help((ctx) => ctx.reply('Send me a message and I will reply!'));
+
+bot.on('message', (ctx) => {
+  ctx.reply('You said: ' + ctx.message.text);
+});
+
+bot.launch();
